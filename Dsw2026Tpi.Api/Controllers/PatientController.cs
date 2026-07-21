9a286dcd03fee66a968a2b9b6acf1f0047eb321a
Marianela Lopez <RocioMarianelaLopez@alu.frt.edu.ar>
2026-07-21 08:36:31 -0300

    feat(patient): crear estructura del módulo paciente

---
+++ Dsw2026Tpi.Api/Controllers/PatientController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Tpi.Api.Controllers
+{
+    public class PatientController
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+
+[HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+
+[HttpPost]
+        public IActionResult Create(...)
+
+[HttpPut("{id}")]
+        public IActionResult Update(...)
+
+[HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+    }
+}
