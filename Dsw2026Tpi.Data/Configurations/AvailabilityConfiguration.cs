5087066884cfa283590c17b07141638986cda30f
Iván Chirino <civanchirino@gmail.com>
2026-07-21 08:43:51 -0300

    Configuraciones EF y DbContext actualizados

---
+++ Dsw2026Tpi.Data/Configurations/AvailabilityConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dsw2026Tpi.Data.Configurations;
+
+public class AvailabilityConfiguration : IEntityTypeConfiguration<Availability>
+{
+    public void Configure(EntityTypeBuilder<Availability> builder)
+    {
+        builder.ToTable("Availabilities");
+    }
+}
--- Dsw2026Tpi.Data/Dsw2026TpiDbContext.cs
+++ Dsw2026Tpi.Data/Dsw2026TpiDbContext.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using Dsw2026Tpi.Domain.Entities;
 
 namespace Dsw2026Tpi.Data;
 
 public class Dsw2026TpiDbContext: DbContext
 {
+    public DbSet<Doctor> Doctors => Set<Doctor>();
+    public DbSet<Speciality> Specialities => Set<Speciality>();
+    public DbSet<Patient> Patients => Set<Patient>();
+    public DbSet<Availability> Availabilities => Set<Availability>();
+    public DbSet<Turn> Turns => Set<Turn>();
+    public DbSet<Appointment> Appointments => Set<Appointment>();
     public Dsw2026TpiDbContext(DbContextOptions<Dsw2026TpiDbContext> options):
         base(options)
     {
