38b9d17bfe37a41df841f0362222cc7f22b9fe62
Iván Chirino <civanchirino@gmail.com>
2026-07-21 08:42:45 -0300

    Entidades Patient, Availability, Turn y Appointment agregadas

---
+++ Dsw2026Tpi.Domain/Entities/Appointment.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Dsw2026Tpi.Domain.Enums;
+
+namespace Dsw2026Tpi.Domain.Entities;
+
+public class Appointment : EntityBase
+{
+    public DateOnly AppointmentDate { get; private set; }
+    public DateOnly? CancellationDate { get; private set; }
+
+    public AppointmentStatus Status { get; private set; }
+
+    public Guid TurnId { get; private set; }
+    public Turn? Turn { get; private set; }
+
+    public Guid PatientId { get; private set; }
+    public Patient? Patient { get; private set; }
+
+    #region Constructor for EF
+#pragma warning disable CS8618
+    private Appointment()
+    {
+    }
+#pragma warning restore CS8618
+    #endregion
+
+    public Appointment(
+        DateOnly appointmentDate,
+        Guid turnId,
+        Guid patientId,
+        Guid? id = null) : base(id)
+    {
+        AppointmentDate = appointmentDate;
+        TurnId = turnId;
+        PatientId = patientId;
+        Status = AppointmentStatus.Confirmed;
+    }
+
+    public void Cancel(DateOnly cancellationDate)
+    {
+        Status = AppointmentStatus.Cancelled;
+        CancellationDate = cancellationDate;
+    }
+
+    public void Complete()
+    {
+        Status = AppointmentStatus.Completed;
+    }
+}
---
+++ Dsw2026Tpi.Domain/Entities/Turn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Dsw2026Tpi.Domain.Enums;
+
+namespace Dsw2026Tpi.Domain.Entities;
+
+public class Turn : EntityBase
+{
+    public DateOnly Date { get; init; }
+    public TimeOnly StartTime { get; init; }
+    public TimeOnly EndTime { get; init; }
+
+    public TurnStatus Status { get; private set; }
+
+    public Guid AvailabilityId { get; private set; }
+    public Availability? Availability { get; private set; }
+
+    #region Constructor for EF
+#pragma warning disable CS8618
+    private Turn()
+    {
+    }
+#pragma warning restore CS8618
+    #endregion
+
+    public Turn(
+        DateOnly date,
+        TimeOnly startTime,
+        TimeOnly endTime,
+        Guid availabilityId,
+        Guid? id = null) : base(id)
+    {
+        Date = date;
+        StartTime = startTime;
+        EndTime = endTime;
+        AvailabilityId = availabilityId;
+        Status = TurnStatus.Available;
+    }
+
+    public void Reserve()
+    {
+        Status = TurnStatus.Reserved;
+    }
+
+    public void Release()
+    {
+        Status = TurnStatus.Available;
+    }
+
+    public void Block()
+    {
+        Status = TurnStatus.Blocked;
+    }
+}
