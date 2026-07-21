86136a18090bdc008c330ccc2b371d3e4f259333
Iván Chirino <civanchirino@gmail.com>
2026-07-21 08:41:23 -0300

    Appointment y TurnStatus agregados

---
+++ Dsw2026Tpi.Domain/Enums/AppointmentStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Domain.Enums
+{
+    public enum AppointmentStatus
+
+    {
+
+        Confirmed,
+
+        Cancelled,
+
+        Completed
+    }
+}
