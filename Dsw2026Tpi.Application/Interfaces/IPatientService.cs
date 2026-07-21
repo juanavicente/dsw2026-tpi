e056cbaed0d849d6a3875786168ab8a1cb77327c
Marianela Lopez <RocioMarianelaLopez@alu.frt.edu.ar>
2026-07-21 09:37:13 -0300

    feat(patient): definir contrato e implementación base del servicio

--- Dsw2026Tpi.Application/Interfaces/IPatientService.cs
+++ Dsw2026Tpi.Application/Interfaces/IPatientService.cs
@@ -10,12 +10,4 @@ namespace Dsw2026Tpi.Application.Interfaces;
 public interface IPatientService
 {
     Task<Pagination<PatientModel.Response>> GetAll(int pageSize, int pageIndex);
-
-    Task<PatientModel.Response?> GetById(Guid id);
-
-    Task<PatientModel.Response> Create(PatientModel.Request request);
-
-    Task<PatientModel.Response> Update(Guid id, PatientModel.Request request);
-
-    Task Delete(Guid id);
 }
--- Dsw2026Tpi.Application/Services/PatientService.cs
+++ Dsw2026Tpi.Application/Services/PatientService.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.Application.Interfaces;
+using Dsw2026Tpi.Domain.Entities;
+using Dsw2026Tpi.Domain.Interfaces;
 
-namespace Dsw2026Tpi.Application.Services
+namespace Dsw2026Tpi.Application.Services;
+
+public class PatientService : IPatientService
 {
-    internal class PatientService
+    private readonly IPersistence _persistence;
+
+    public PatientService(IPersistence persistence)
     {
+        _persistence = persistence;
+    }
+
+    public async Task<Pagination<PatientModel.Response>> GetAll(int pageSize, int pageIndex)
+    {
+        throw new NotImplementedException();
     }
 }
