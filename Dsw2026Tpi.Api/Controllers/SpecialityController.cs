a49348881b4b78c2e06d460c8b21057b3e8f5afd
Juana Vicente <juanivicentee@gmail.com>
2026-07-29 17:45:23 -0300

    Actualizar especialidades según TPI 1.3

--- Dsw2026Tpi.Api/Controllers/SpecialityController.cs
+++ Dsw2026Tpi.Api/Controllers/SpecialityController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Dsw2026Tpi.Api.Controllers;
 
-[Route("specialities")]
+[Route("api/specialties")]
 [Authorize(Policy = Policies.AdminPolicy)]
 public class SpecialityController : AppController
 {
