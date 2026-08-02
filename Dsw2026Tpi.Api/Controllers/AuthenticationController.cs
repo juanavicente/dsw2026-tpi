6cf6b2d5fb3ac7633dead5bb7ce6e79280fd7453
Marianela Lopez <RocioMarianelaLopez@alu.frt.edu.ar>
2026-08-02 20:53:52 -0300

    Corrección del endpoint Autorización

--- Dsw2026Tpi.Api/Controllers/AuthenticationController.cs
+++ Dsw2026Tpi.Api/Controllers/AuthenticationController.cs
@@ -14,15 +14,6 @@ public class AuthenticationController : AppController
         _authenticationService = authenticationService;
     }
 
-    [HttpPost("admin/register")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> Register([FromBody] RegisterModel.Request request)
-    {
-        var result = await _authenticationService.Register(request);
-        return Ok(result.Email);
-    }
-
     [HttpPost("admin/login")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
---
+++ SQLQuery3.sql
@@ -0,0 +1,7 @@
+﻿SELECT 
+    u.Email,
+    r.Name AS Rol
+FROM Users u
+LEFT JOIN UsersRoles ur ON u.Id = ur.UserId
+LEFT JOIN Roles r ON ur.RoleId = r.Id
+WHERE u.Email = 'desarrollo@test.com';
