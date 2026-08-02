9ca3790ce712ce109c2271dca9133bed700498a1
Iván Chirino <civanchirino@gmail.com>
2026-08-02 19:02:24 -0300

    Corrige índice único de especialidades para borrado lógico

---
+++ Dsw2026Tpi.Data/Migrations/20260802212421_SpecialitySoftDeleteIndex.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dsw2026Tpi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SpecialitySoftDeleteIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Specialities_Name",
+                table: "Specialities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Specialities_Name",
+                table: "Specialities",
+                column: "Name",
+                unique: true,
+                filter: "[IsDeleted]=0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Specialities_Name",
+                table: "Specialities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Specialities_Name",
+                table: "Specialities",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
